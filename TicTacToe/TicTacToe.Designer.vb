e3ff9897d19fd36f561717f6ee89776466f65556
Kyle <kyle.chapman@durhamcollege.ca>
2020-02-11 14:05:05 -0500

    Gotta commit to sync this stuff!

---
+++ TicTacToe/TicTacToe.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmTicTacToe
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnRow0Column0 = New System.Windows.Forms.Button()
+        Me.btnRow0Column1 = New System.Windows.Forms.Button()
+        Me.btnRow0Column2 = New System.Windows.Forms.Button()
+        Me.lblGameStatus = New System.Windows.Forms.Label()
+        Me.btnRow1Column2 = New System.Windows.Forms.Button()
+        Me.btnRow1Column1 = New System.Windows.Forms.Button()
+        Me.btnRow1Column0 = New System.Windows.Forms.Button()
+        Me.btnRow2Column2 = New System.Windows.Forms.Button()
+        Me.btnRow2Column1 = New System.Windows.Forms.Button()
+        Me.btnRow2Column0 = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.ttpTipTacToe = New System.Windows.Forms.ToolTip(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnRow0Column0
+        '
+        Me.btnRow0Column0.Location = New System.Drawing.Point(12, 50)
+        Me.btnRow0Column0.Name = "btnRow0Column0"
+        Me.btnRow0Column0.Size = New System.Drawing.Size(50, 50)
+        Me.btnRow0Column0.TabIndex = 0
+        Me.btnRow0Column0.UseVisualStyleBackColor = True
+        '
+        'btnRow0Column1
+        '
+        Me.btnRow0Column1.Location = New System.Drawing.Point(69, 50)
+        Me.btnRow0Column1.Name = "btnRow0Column1"
+        Me.btnRow0Column1.Size = New System.Drawing.Size(50, 50)
+        Me.btnRow0Column1.TabIndex = 1
+        Me.btnRow0Column1.UseVisualStyleBackColor = True
+        '
+        'btnRow0Column2
+        '
+        Me.btnRow0Column2.Location = New System.Drawing.Point(126, 50)
+        Me.btnRow0Column2.Name = "btnRow0Column2"
+        Me.btnRow0Column2.Size = New System.Drawing.Size(50, 50)
+        Me.btnRow0Column2.TabIndex = 2
+        Me.btnRow0Column2.UseVisualStyleBackColor = True
+        '
+        'lblGameStatus
+        '
+        Me.lblGameStatus.Location = New System.Drawing.Point(12, 24)
+        Me.lblGameStatus.Name = "lblGameStatus"
+        Me.lblGameStatus.Size = New System.Drawing.Size(164, 23)
+        Me.lblGameStatus.TabIndex = 3
+        Me.lblGameStatus.Text = "X's Turn"
+        Me.lblGameStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnRow1Column2
+        '
+        Me.btnRow1Column2.Location = New System.Drawing.Point(126, 106)
+        Me.btnRow1Column2.Name = "btnRow1Column2"
+        Me.btnRow1Column2.Size = New System.Drawing.Size(50, 50)
+        Me.btnRow1Column2.TabIndex = 6
+        Me.btnRow1Column2.UseVisualStyleBackColor = True
+        '
+        'btnRow1Column1
+        '
+        Me.btnRow1Column1.Location = New System.Drawing.Point(69, 106)
+        Me.btnRow1Column1.Name = "btnRow1Column1"
+        Me.btnRow1Column1.Size = New System.Drawing.Size(50, 50)
+        Me.btnRow1Column1.TabIndex = 5
+        Me.btnRow1Column1.UseVisualStyleBackColor = True
+        '
+        'btnRow1Column0
+        '
+        Me.btnRow1Column0.Location = New System.Drawing.Point(12, 106)
+        Me.btnRow1Column0.Name = "btnRow1Column0"
+        Me.btnRow1Column0.Size = New System.Drawing.Size(50, 50)
+        Me.btnRow1Column0.TabIndex = 4
+        Me.btnRow1Column0.UseVisualStyleBackColor = True
+        '
+        'btnRow2Column2
+        '
+        Me.btnRow2Column2.Location = New System.Drawing.Point(126, 162)
+        Me.btnRow2Column2.Name = "btnRow2Column2"
+        Me.btnRow2Column2.Size = New System.Drawing.Size(50, 50)
+        Me.btnRow2Column2.TabIndex = 9
+        Me.btnRow2Column2.UseVisualStyleBackColor = True
+        '
+        'btnRow2Column1
+        '
+        Me.btnRow2Column1.Location = New System.Drawing.Point(69, 162)
+        Me.btnRow2Column1.Name = "btnRow2Column1"
+        Me.btnRow2Column1.Size = New System.Drawing.Size(50, 50)
+        Me.btnRow2Column1.TabIndex = 8
+        Me.btnRow2Column1.UseVisualStyleBackColor = True
+        '
+        'btnRow2Column0
+        '
+        Me.btnRow2Column0.Location = New System.Drawing.Point(12, 162)
+        Me.btnRow2Column0.Name = "btnRow2Column0"
+        Me.btnRow2Column0.Size = New System.Drawing.Size(50, 50)
+        Me.btnRow2Column0.TabIndex = 7
+        Me.btnRow2Column0.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnReset.Location = New System.Drawing.Point(12, 228)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(164, 23)
+        Me.btnReset.TabIndex = 10
+        Me.btnReset.Text = "&Reset Game"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'frmTicTacToe
+        '
+        Me.AcceptButton = Me.btnRow1Column1
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnReset
+        Me.ClientSize = New System.Drawing.Size(192, 283)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnRow2Column2)
+        Me.Controls.Add(Me.btnRow2Column1)
+        Me.Controls.Add(Me.btnRow2Column0)
+        Me.Controls.Add(Me.btnRow1Column2)
+        Me.Controls.Add(Me.btnRow1Column1)
+        Me.Controls.Add(Me.btnRow1Column0)
+        Me.Controls.Add(Me.lblGameStatus)
+        Me.Controls.Add(Me.btnRow0Column2)
+        Me.Controls.Add(Me.btnRow0Column1)
+        Me.Controls.Add(Me.btnRow0Column0)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmTicTacToe"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tic Tac Toe"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnRow0Column0 As Button
+    Friend WithEvents btnRow0Column1 As Button
+    Friend WithEvents btnRow0Column2 As Button
+    Friend WithEvents lblGameStatus As Label
+    Friend WithEvents btnRow1Column2 As Button
+    Friend WithEvents btnRow1Column1 As Button
+    Friend WithEvents btnRow1Column0 As Button
+    Friend WithEvents btnRow2Column2 As Button
+    Friend WithEvents btnRow2Column1 As Button
+    Friend WithEvents btnRow2Column0 As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents ttpTipTacToe As ToolTip
+End Class
---
+++ TicTacToe/TicTacToe.vb
@@ -0,0 +1,192 @@
+﻿' Last Modified By:   
+' Last Modified Date: 
+'
+' Author: Kyle Chapman
+' Date: February 9, 2020
+' Description:
+'   It's a tic-tac-toe game! Is that supposed to be hypenated?
+'   There are 9 buttons in a 2D array. When clicked, they are assigned
+'   a letter based on whoever's turn it is and disabled.
+
+Public Class frmTicTacToe
+
+    ' This boolean value shows whether it's X's turn; otherwise it's O's turn
+    Dim isTurnX As Boolean = True
+    ' This boolean value represents the win condition
+    Dim isWinner As Boolean = False
+    ' Here's an array of buttons, representing squares on the board
+    Dim squares As Button(,)
+
+#Region "Event Handlers"
+
+    ''' <summary>
+    ''' On form load, assign the Buttons to the array
+    ''' </summary>
+    Private Sub frmTicTacToe_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        squares = {
+    {btnRow0Column0, btnRow0Column1, btnRow0Column2},
+    {btnRow1Column0, btnRow1Column1, btnRow1Column2},
+    {btnRow2Column0, btnRow2Column1, btnRow2Column2}}
+    End Sub
+
+    ''' <summary>
+    ''' When a button is clicked, assign it a letter, disable the button, and if there was no winner change the turn
+    ''' </summary>
+    Private Sub TicTacToeSquareClick(txtSender As Button, e As EventArgs) Handles btnRow0Column0.Click, btnRow0Column1.Click, btnRow0Column2.Click, btnRow1Column0.Click, btnRow1Column1.Click, btnRow1Column2.Click, btnRow2Column0.Click, btnRow2Column1.Click, btnRow2Column2.Click
+
+        ' Set the text on the Button
+        If isTurnX Then
+            txtSender.Text = "X"
+        Else
+            txtSender.Text = "O"
+        End If
+
+        ' Disable the button
+        txtSender.Enabled = False
+
+        ' Evaluate win
+        EvaluateWin()
+
+        ' Don't swap turns if somebody has won!
+        If Not isWinner Then
+            ' Swap turns
+            ChangeTurn()
+        End If
+
+        ' Put focus on the Reset button
+        btnReset.Focus()
+
+    End Sub
+
+    ''' <summary>
+    ''' The Reset button clears things and re-enables controls
+    ''' </summary>
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        SetDefaults()
+
+    End Sub
+
+#End Region
+
+#Region "Procedures"
+
+    ''' <summary>
+    ''' This clears the text property of all controls in the array of controls that is passed in
+    ''' </summary>
+    ''' <param name="controlArray">An array of controls with a text property to clear</param>
+    Sub ClearControls(controlArray As Control(,))
+
+        ' For every control in the list that is passed in, empty its Text property
+        For Each controlToClear As Control In controlArray
+            controlToClear.Text = String.Empty
+        Next
+
+    End Sub
+
+    ''' <summary>
+    ''' This enables or disables all controls in the array that is passed in
+    ''' </summary>
+    ''' <param name="controlArray">An array of controls to enable or disable</param>
+    ''' <param name="enabledStatus">Boolean: set controls to enabled?</param>
+    Sub SetControlsEnabled(controlArray As Control(,), enabledStatus As Boolean)
+
+        ' For every textbox in the list that is passed in, set its Enabled property
+        For Each controlToSet As Button In controlArray
+            controlToSet.Enabled = enabledStatus
+        Next
+
+    End Sub
+
+    ''' <summary>
+    ''' Clears things and re-enables controls
+    ''' </summary>
+    Sub SetDefaults()
+
+        ' Clear all buttons
+        ClearControls(squares)
+        ' Re-enable any controls that may be disabled
+        SetControlsEnabled(squares, True)
+        ' Ensure there's no winner
+        isWinner = False
+        ' ChangeTurn resets the status label and gives the loser the first turn
+        ChangeTurn()
+
+    End Sub
+
+    ''' <summary>
+    ''' If it's currently X's turn, change it to O's turn, and vice-versa
+    ''' </summary>
+    Sub ChangeTurn()
+
+        isTurnX = Not isTurnX
+
+        If isTurnX Then
+            lblGameStatus.Text = "X's Turn"
+        Else
+            lblGameStatus.Text = "O's Turn"
+        End If
+
+    End Sub
+
+    ''' <summary>
+    ''' Determine if the current player has won
+    ''' </summary>
+    Sub EvaluateWin()
+
+        ' **************************************
+        ' This is the part where you code stuff!
+        ' **************************************
+
+        ' First, check all rows.
+        ' Use a For loop to check if all boxes in the same dimension
+        ' have the same value - doesn't even matter what the value is
+        ' as long as it's the same!
+        For rowCount As Integer = 0 To squares.GetUpperBound(0)
+            ' What does "GetUpperBound(0)" mean above?
+
+            ' Something to do with squares(rowCount,0), squares(rowCount,1), etc.
+        Next
+
+        ' Next, check all columns.
+
+        ' Finally, we have to check the two diagonals.
+        ' It is not going to save us any work to use loops
+        ' so let's just do it like this:
+        If squares(0, 0).Text <> String.Empty And
+            squares(0, 0).Text = squares(1, 1).Text And
+            squares(0, 0).Text = squares(2, 2).Text Then
+            isWinner = True
+        ElseIf squares(0, 2).Text <> String.Empty And
+            squares(0, 2).Text = squares(1, 1).Text And
+            squares(0, 2).Text = squares(2, 0).Text Then
+            isWinner = True
+        End If
+
+        ' If you want to get fancy...
+        ' make the winning line show up in a different colour!
+
+        ' If you want to get *super* fancy...
+        ' how might you code this so it would work for boards of an arbitrary and bigger (e.g. 4x4) size?
+
+        ' *********************************************
+        ' This is the part where you stop coding stuff!
+        ' *********************************************
+
+        ' If there's a winner, show who it is!
+        If isWinner Then
+            If isTurnX Then
+                lblGameStatus.Text = "X wins!"
+            Else
+                lblGameStatus.Text = "O wins!"
+            End If
+
+            ' Disable all squares to prevent further play
+            SetControlsEnabled(squares, False)
+        End If
+
+    End Sub
+
+#End Region
+
+End Class
